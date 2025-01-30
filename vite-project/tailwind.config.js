8f4f108a430b5a2b3cb3e86051fb58efd3c0bc2e
Szczepaniak_Karolina <karolina.szczepaniak468@gmail.com>
2025-01-30 10:58:38 +0100

    some_new

---
+++ vite-project/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "main.jsx","./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
